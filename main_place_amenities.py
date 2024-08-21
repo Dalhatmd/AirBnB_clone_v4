bf294cb6f2ed503374eba057a2504e9b18161b01
Dalhatmd <umarwalid4055@gmail.com>
2024-08-21 17:52:34 +0100

    Add a few things to populate web page

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Test link Many-To-Many Place <> Amenity
 """
-from models import *# creation of a State
+from models import *
 state = State(name="California")
 state.save()
 
